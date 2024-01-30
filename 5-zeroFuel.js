525498fe0cf51a1596384c23a256e9b99176ed16
rafaelsety <mhmmdrafael28@gmail.com>
2024-01-30 18:11:59 +0700

    5/366

---
+++ 5-zeroFuel.js
@@ -0,0 +1,24 @@
+// 2024 Coding Challenge
+// 5/366
+// https://www.codewars.com/kata/5861d28f124b35723e00005e
+
+const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
+  if (distanceToPump <= mpg * fuelLeft) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+// const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
+//   return distanceToPump <= mpg * fuelLeft;
+// };
+
+// const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
+//   return distanceToPump / mpg <= fuelLeft;
+// }
+
+// const zeroFuel = (distanceToPump, mpg, fuelLeft) => distanceToPump <= mpg * fuelLeft;
+
+
+console.log(zeroFuel(50, 25, 2));

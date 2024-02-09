a360d889b964fee45776ee28b7653852d967adf3
rafaelsety <mhmmdrafael28@gmail.com>
2024-02-09 18:24:16 +0700

    12/366

---
+++ 12-arrayPlusArray.js
@@ -0,0 +1,28 @@
+// 2024 Coding Challenge
+// 12/366
+// https://www.codewars.com/kata/5a2be17aee1aaefe2a000151
+
+// function arrayPlusArray(arr1, arr2) {
+//     let result = 0;
+//     for (let i = 0; i < arr1.length; i++) {
+//         result += arr1[i];
+//     }
+//     for (let i = 0; i < arr2.length; i++) {
+//         result += arr2[i];
+//     }
+//     return result;
+// }
+
+// function arrayPlusArray(arr1, arr2) {
+//     let result = 0;
+//     for (let i = 0; i < arguments.length; i++) {
+//         for (let j = 0; j < arguments[i].length; j++) {
+//             result += arguments[i][j];
+//         }
+//     }
+//     return result;
+// }
+
+const arrayPlusArray = (arr1, arr2) => [...arr1, ...arr2].reduce((acc, curr) => acc + curr);
+
+console.log(arrayPlusArray([1, 2, 3], [4, 5, 6]))

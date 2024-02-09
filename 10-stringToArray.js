cf945dee001300b8aad11c5db2020c89e65ab978
rafaelsety <mhmmdrafael28@gmail.com>
2024-02-09 16:37:27 +0700

    10/366

---
+++ 10-stringToArray.js
@@ -0,0 +1,12 @@
+// 2024 Coding Challenge
+// 9/366
+// https://www.codewars.com/kata/57e76bc428d6fbc2d500036d
+
+// function stringToArray(string) {
+//     let result = string.split(" ");
+//     return result;
+// }
+
+const stringToArray = (string) => string.split(' ');
+
+console.log(stringToArray("I love arrays they are my favorite"));
